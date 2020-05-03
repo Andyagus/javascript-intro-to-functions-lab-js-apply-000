9ebf3eb306cb31883341b49ad1f18cc2751d5f75
Andy Agus <redagus@gmail.com>
2020-05-03 19:14:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string){
+  console.log ("Hello".toUpperCase())
+}
+
+function logWhisper(string){
+  console.log ("Hello".toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()===string) {
+      console.log ("\"I can/'t hear you\"")
+  }
+  else if (string.toLowerCase()===string) {
+      console.log ("YES INDEED!")
+  }
+}
