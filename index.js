c8de16a9c01a950153da7014330f0da13023106a
Andy Agus <redagus@gmail.com>
2020-05-03 19:15:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ function logWhisper(string){
   console.log ("Hello".toLowerCase())
 }
 
-var string = "hello"
-function sayHiToGrandma(string) {
-  if (string.toLowerCase()===string) {
+function sayHiToGrandma(num1) {
+  if (num1.toLowerCase()===num) {
       console.log ("\"I can/'t hear you\"")
   }
-  else if (string.toLowerCase()===string) {
+  else if (num1.toLowerCase()===num) {
       console.log ("YES INDEED!")
   }
 }
+
+sayHiToGrandma()
